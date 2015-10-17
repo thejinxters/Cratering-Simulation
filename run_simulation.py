b6a6340b9b2dc196b27bbc51cf21f78f8a24d32e
Brandon Mikulka <brandon.mikulka@colorado.edu>
2015-10-17 15:45:34 -0600

    Move simulation to its own function

--- run_simulation.py
+++ run_simulation.py
@@ -4,6 +4,14 @@ import time
 
 
 def main():
+    year, saturation, saturation_per_year = simulation()
+
+    print "Years: " + str(year) + "000"
+    print "Crater Saturation: " + str(saturation)
+    print saturation_per_year
+
+
+def simulation():
     #Create some uniformly random locations
     x_locations = UniformRandomNumbers(0, 500, 5000)
     y_locations = UniformRandomNumbers(0, 500, 5000)
@@ -15,7 +23,7 @@ def main():
     not_saturated = True
     while not_saturated:
         year += 1
-        #time.sleep(.1) #slow down the simulation to see what is happening
+        #time.sleep(.1) #enable to slow simulation
         crater = Crater(x_locations.get_next(), y_locations.get_next())
         surface.add_crater(crater)
 
@@ -24,12 +32,13 @@ def main():
         old_saturation = saturation_per_year[year/2]
         if old_saturation != 0:
             change_percent = new_saturation / (old_saturation  * 1.0) - 1
-            print change_percent
             if (change_percent < 0.05):
-                print new_saturation
+                saturation_per_year[year] = new_saturation
                 break
 
         saturation_per_year[year] = new_saturation
+
+    return (year, new_saturation, saturation_per_year)
 
 
 
